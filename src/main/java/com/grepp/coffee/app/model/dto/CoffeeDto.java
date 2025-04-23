e18d524f29b82c1ae9318fa2216c91e6312f7a5e
DY-Tempus <2744ch@naver.com>
2025-04-23 18:46:48 +0900

    :bug: fix: Update OrderMapper, DTO Classes

--- src/main/java/com/grepp/coffee/app/model/dto/CoffeeDto.java
+++ src/main/java/com/grepp/coffee/app/model/dto/CoffeeDto.java
@@ -1,14 +1,14 @@
 package com.grepp.coffee.app.model.dto;
 
 import lombok.AllArgsConstructor;
-import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Alias("CoffeeDto")
 public class CoffeeDto {
 
     private Integer coffeeId;
