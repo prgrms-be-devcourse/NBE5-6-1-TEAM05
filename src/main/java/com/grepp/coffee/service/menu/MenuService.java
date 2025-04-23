81012b82207d343fcae7a4648902620d18c46aa9
IDIOcoder <newon.jsw@gmail.com>
2025-04-23 15:11:47 +0900

    :bulb: remove useless comment

--- src/main/java/com/grepp/coffee/service/menu/MenuService.java
+++ src/main/java/com/grepp/coffee/service/menu/MenuService.java
@@ -4,7 +4,7 @@ import com.grepp.coffee.app.model.dto.CoffeeDto;
 import java.util.List;
 
 public interface MenuService {
-    void addMenu(CoffeeDto coffeeDto);  // 커피 메뉴 추가
+    void addMenu(CoffeeDto coffeeDto);
     List<CoffeeDto> getAllCoffee();
     void updateMenu(CoffeeDto coffeeDto);
     void deleteMenu(CoffeeDto coffeeDto);
