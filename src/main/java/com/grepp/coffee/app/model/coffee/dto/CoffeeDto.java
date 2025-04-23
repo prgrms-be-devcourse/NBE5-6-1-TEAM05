9c8bbc4f82a61cbf03bf42c75bd5093b0ed3fcd9
DY-Tempus <2744ch@naver.com>
2025-04-23 14:20:07 +0900

    :sparkles: feat: ADD Mapper

--- src/main/java/com/grepp/coffee/app/model/coffee/dto/CoffeeDto.java
+++ src/main/java/com/grepp/coffee/app/model/coffee/dto/CoffeeDto.java
@@ -1,14 +1,17 @@
 package com.grepp.coffee.app.model.coffee.dto;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class CoffeeDto {
 
     private Integer coffeeId;
+    private String coffeeName;
     private Integer price;
     private Integer stock;
 
---
+++ src/main/java/com/grepp/coffee/app/model/coffee/repository/mapper/CoffeeMapper.java
@@ -0,0 +1,5 @@
+package com.grepp.coffee.app.model.coffee.repository.mapper;
+
+public interface CoffeeMapper {
+
+}
