e938321379dae38e51e72795f04c4b2b9def77b8
Kminseo <childstone@users.noreply.github.com>
2025-04-23 21:39:14 +0900

    ::sparkles:: feat : add OrderControllers
    web and api

---
+++ src/main/java/com/grepp/coffee/app/controller/web/payload/OrderRequest.java
@@ -0,0 +1,19 @@
+package com.grepp.coffee.app.controller.web.payload;
+
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class OrderRequest {
+
+    // 커피 종류와 수량은 session에 저장된 값을 사용
+    // 고객 email과 주소, 우편번호만 받기
+
+    @Email
+    private String email;
+    private String address;
+    private Integer postNum;
+
+}
