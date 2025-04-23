dffda774841ade7c69e4513199e8696b1827bef2
DY-Tempus <2744ch@naver.com>
2025-04-23 14:20:07 +0900

    :sparkles: feat: ADD Mapper

--- src/main/java/com/grepp/coffee/app/model/dto/CoffeeDto.java
+++ src/main/java/com/grepp/coffee/app/model/dto/CoffeeDto.java
@@ -2,6 +2,7 @@ package com.grepp.coffee.app.model.dto;
 
 import lombok.AllArgsConstructor;
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
