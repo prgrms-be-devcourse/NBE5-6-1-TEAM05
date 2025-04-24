09d2554729fb609e61441ac1b5dc9d7c5c0b7048
IDIOcoder <newon.jsw@gmail.com>
2025-04-24 17:40:24 +0900

    :recycle: refactor processOrder features
    - OrderRepository 에서 기존의 주문 처리 매서드(saveOrderSmart)를 세부적인 3개의 매서드로 분할했습니다.
    - OrderRepository 에서 saveOrderSmart 에 구현되었던 로직을 OrderService 의 processOrder 로 옮겼습니다.

--- src/main/java/com/grepp/coffee/app/service/OrderService.java
+++ src/main/java/com/grepp/coffee/app/service/OrderService.java
@@ -15,10 +15,36 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    /** 고객이 주문을 확정 했을 때 해당 주문을 DB에 반영합니다. */
+    /**
+     * 고객이 주문을 확정 했을 때 해당 주문을 DB에 반영합니다.
+     * 만약 고객이 같은 주소로 주문했을 경우, 기존의 주문 정보를 업데이트합니다.
+     * */
     @Transactional
     public boolean processOrder(OrderDto orderDto, List<DetailedOrderDto> detailedOrderLists) {
-        return orderRepository.saveOrderSmart(orderDto, detailedOrderLists);
+        OrderDto existingOrder = orderRepository.findTodayOrder(orderDto.getEmail(), orderDto.getPostNum());
+
+        if (existingOrder == null) { // 새로운 주문 생성
+            boolean created = orderRepository.insertOrder(orderDto);
+            if(!created) {
+                return false;
+            }
+
+            for (DetailedOrderDto item : detailedOrderLists) {
+                item.setOrderNum(orderDto.getOrderNum());
+                if(!orderRepository.insertDetailAndDecreaseStock(item)) {
+                    return false;
+                }
+            }
+        } else {    // 기존 주문에 상세 주문만 추가
+            for (DetailedOrderDto item : detailedOrderLists) {
+                item.setOrderNum(existingOrder.getOrderNum());
+                if (!orderRepository.insertDetailAndDecreaseStock(item)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     /** 고객이 주문을 취소한 경우 해당 취소 내역을 DB에 반영합니다. */
