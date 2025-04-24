092f5b45209ced824b83af539cd18e27c6e9216f
IDIOcoder <newon.jsw@gmail.com>
2025-04-24 14:37:30 +0900

    :bulb: Update comments
    - 단순히 주석을 정리했습니다.

--- src/main/java/com/grepp/coffee/app/service/MenuService.java
+++ src/main/java/com/grepp/coffee/app/service/MenuService.java
@@ -13,30 +13,29 @@ public class MenuService {
 
     private final CoffeeRepository coffeeRepository;
 
-    // DB로 새로운 커피 메뉴의 추가요청을 보냅니다.
+    /** DB에 새로운 커피 메뉴를 추가합니다. */
     @Transactional
     public boolean addMenu(CoffeeDto coffeeDto) {
-        // 커피 이름, 가격, 재고가 전달 되야함.
         return coffeeRepository.insertCoffee(coffeeDto);
     }
 
-    // DB에 존재하는 모든 커피 메뉴들을 가져옵니다.
+    /** DB로부터 모든 커피 메뉴들을 가져옵니다. */
     public List<CoffeeDto> getAllCoffee() {
         return coffeeRepository.getAllCoffee();
     }
 
-    // DB로부터 특정 커피에 대한 데이터를 가져옵니다.
+    /** DB로부터 특정 커피 메뉴에 대한 데이터를 가져옵니다. */
     public CoffeeDto getCoffee(CoffeeDto coffeeDto) {
         return coffeeRepository.getCoffeeById(coffeeDto.getCoffeeId());
     }
 
-    // DB에 저장된 커피 메뉴를 수정합니다.
+    /** DB에 저장된 커피 메뉴를 수정합니다. */
     @Transactional
     public boolean updateMenu(CoffeeDto coffeeDto) {
         return coffeeRepository.updateCoffee(coffeeDto);
     }
 
-    // DB에 저장된 커피메뉴를 삭제합니다.
+    /** DB에 저장된 커피메뉴를 삭제합니다. */
     @Transactional
     public boolean deleteMenu(CoffeeDto coffeeDto) {
         return coffeeRepository.deleteCoffee(coffeeDto.getCoffeeId());
